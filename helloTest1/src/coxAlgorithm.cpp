c9239bb998c1e485646fa3de303172cdaae86f7e
roastbread <ernil.lunda@gmail.com>
2023-04-27 14:27:51 +0200

    cox runs but is bad, fix median

--- helloTest1/src/coxAlgorithm.cpp
+++ helloTest1/src/coxAlgorithm.cpp
@@ -14,17 +14,22 @@
 //#include <math.h>
 #include <string>
 
+using namespace std;
 using namespace Eigen;
+
 class coxMap{
 	public:
-	//float dist[400];
-	//float ang[400];
+	double dists[400];
+	double angs[400];
 	//double sensorPose[] = {1000000,20000000,3000000};
-	Eigen::Matrix<double, 1, 400> x;
-	Eigen::Matrix<double, 1, 400> y;
-	Eigen::Matrix<double, 1, 400> DIS;
-	Eigen::Matrix<double, 1, 400> ang;
-	double sensorPose[3] = {660 ,0,-90*M_PI/180};
+	MatrixXd x;
+	MatrixXd y;
+
+	MatrixXd DIS;
+	MatrixXd ang;
+
+	//double sensorPose[3] = {660 ,0,-90*M_PI/180};
+	double sensorPose[3] = {0, 0, 0};
 	float prevX = 0;
 	float prevY = 0;
 	float prevA = 0;
@@ -32,6 +37,20 @@ class coxMap{
 	double ddx = 0;
 	double ddy = 0;
 	double dda = 0;
+
+	MatrixXd line {
+		  {559, 1140, 2856, -910},
+		  {2856, -910, 1336, -2670},
+		  {1336, -2670, -1004, -610},
+		  {-1004, -610, 559, 1140},
+	};
+	MatrixXd rotMat{
+		{0, -1},
+		{1, 0},
+	};
+	Vector2d Li[4];
+	Vector2d ui[4];
+	float ri[4];
 
 	void setup(){
 		for (int i=0; i<line.rows(); i++){
@@ -42,19 +61,6 @@ class coxMap{
 			}
 	}
 
-	Eigen::Matrix<float, 4, 4> line {
-		  {559, -1140, 2856, 910},
-		  {2856, 910, 1336, 2670},
-		  {1336, 2670, -1004, 610},
-		  {-1004, 610, 559, -1140},
-	};
-	Eigen::Matrix<float, 2, 2> rotMat{
-		{0, -1},
-		{1, 0},
-	};
-	Eigen::Vector2<float> Li[4];
-	Eigen::Vector2<float> ui[4];
-	float ri[4];
 
 	void gather_data(){
 		std::string line;
@@ -73,10 +79,15 @@ class coxMap{
 				angle = line.substr(pos1+1,pos2-2);
 				distance = line.substr(pos2+1);
 				//std::cout << quality << "|" << angle << "|" << distance << "|" << i << std::endl;
-				ang(0,i-1) = std::stod(angle);
-				DIS(0,i-1) = std::stod(distance);
+				angs[i-1] = std::stod(angle);
+				dists[i-1] = std::stod(distance);
+
 			}
 		}
+		cout << sizeof(angs)/8 << endl;
+		ang = Eigen::Map<MatrixXd>(angs, 1, 400);
+		DIS = Eigen::Map<MatrixXd>(dists, 1, 400);
+
 		//std::remove("testfile.txt");
 		data.close();
 		//std::ofstream empty;
@@ -89,172 +100,192 @@ class coxMap{
         	//x = x.array();
         	//y = y.array();
             //sensor to Cartesian coordinates
-        	std::cout << "hej" << std::endl;
-            x = DIS.array() * cos(ang.array());
-            y = DIS.array() * sin(ang.array());
+        	//std::cout << "hej" << std::endl;
+        	//std::cout << DIS << std::endl;
+            x = DIS.array() * cos(ang.array()*M_PI/180);
+            y = DIS.array() * sin(ang.array()*M_PI/180);
             //sensor to Robot coordinates
+            //std::cout << x << std::endl;
+            //std::cout << x.cols() << std::endl;
+            //std::cout << y << std::endl;
+            //std::cout << y.cols() << std::endl;
 
-            Eigen::Matrix<double, 3, 3> rotMatRobot {
-                    {cos(sensorPose[3]), -1*sin(sensorPose[3]), sensorPose[1]},
-                    {sin(sensorPose[3]), cos(sensorPose[3]), sensorPose[2]},
+            MatrixXd rotMatRobot {
+                    {cos(sensorPose[2]), -1*sin(sensorPose[2]), sensorPose[0]},
+                    {sin(sensorPose[2]), cos(sensorPose[2]), sensorPose[1]},
                     {0, 0, 1},
             };
 
-            int n = x.cols();
-            Eigen::Matrix<double, 3, 400> xy1T;
+            //std::cout << x.row(0) << std::endl;
+            MatrixXd xy1T(3,x.cols());
             xy1T.row(0) = x;
             xy1T.row(1) = y;
-            xy1T.row(2).setZero();
-            std::cout << xy1T << std::endl;
+            xy1T.row(2).setOnes();
 
 
-            Eigen::Matrix<double, 3, 400> Xs;
+
+            MatrixXd Xs;
 
             Xs = rotMatRobot*xy1T;
 
+
             //Robot coordinates to world coordinates
-            Eigen::Matrix<double, 3, 3> rotMatWorld {
-                    {cos(prevA), -1*sin(prevA), prevX},
-                    {sin(prevA), cos(prevA), prevY},
+            MatrixXd rotMatWorld {
+                    {cos(prevA+dda), -1*sin(prevA+dda), prevX+ddx},
+                    {sin(prevA+dda), cos(prevA+dda), prevY+ddy},
                     {0, 0, 1},
             };
 
-            Eigen::Matrix<double, 3, 400> Xw;
-
+            MatrixXd Xw;
             Xw = rotMatWorld*Xs;
 
             // Step 2 Find targets for data points
             // V is the number of points from the laser scan
 
-            Eigen::Matrix<double, 2, 400 > V;
-            V.row(0) = Xw.row(0);
-            V.row(1) = Xw.row(1);
-            V.transposeInPlace();
+            MatrixXd V(2,x.cols());
+			V.row(0) = Xw.row(0);
+			V.row(1) = Xw.row(1);
 
-            Eigen::Matrix<float, 2, 400 > dist_All;
-            dist_All.row(0).setZero();
-            dist_All.row(1).setZero();
-            dist_All.transposeInPlace();
+			MatrixXd dist_All(2,x.cols());
+			dist_All.row(0).setZero();
+			dist_All.row(1).setZero();
 
-            Eigen::Matrix<float, 2, 400 > qi; //qi == u1 från matlab cox
-            qi.row(0).setZero();
-            qi.row(1).setZero();
-            qi.transposeInPlace();
+			MatrixXd qi(2,x.cols()); //qi == u1 från matlab cox
+			qi.row(0).setZero();
+			qi.row(1).setZero();
 
-            Eigen::Matrix<float, 1, 400 > r;
-            r.row(0).setZero();
-            r.transposeInPlace();
+			MatrixXd r(1,x.cols());
+			r.row(0).setZero();
 
-            Eigen::Matrix<float, 1, 400 > yi;
-            yi.row(0).setZero();
-            yi.transposeInPlace();
+			MatrixXd yi(1,x.cols());
+			yi.row(0).setZero();
 
-            for (int i = 0; i < V.cols(); i++) {
-                Eigen::Matrix<float, 1, 4 > dist;
-                for (int j = 0; i < line.cols(); i++) {
-                    // Tar ut distansen från laser scan till väggarna
-                    dist(j) = pointLineDistance(V(0, i), V(1, i), line(j, 0), line(j, 1), line(j, 2), line(j, 3));
-                }
-                dist.transposeInPlace();
+			for (int k = 0; k < V.cols(); k++) {
+				VectorXd dist(line.rows());
+				for (int j = 0; j < line.cols(); j++) {
+					// Tar ut distansen från laser scan till väggarna
+					dist(j) = pointLineDistance(V(0, k), V(1, k), line(j, 0), line(j, 1), line(j, 2), line(j, 3));
+				}
 
-                Eigen::Index index;
-                double minVal = dist.minCoeff(&index);
+				Index index;
+				double minVal = dist.minCoeff(&index);
 
-                dist_All.col(i) << minVal, index;
-                qi.col(i) = qi.row(index).transpose();
+				dist_All.col(k) << minVal, index;
+				qi.col(k) = ui[index];
 
-                Eigen::VectorXd z = line.row(index).head<2>();
-                for (int i = 0; i < 400; i++) {
-                    r(i) = qi.col(i)*z;
-                }
+				//z
+				VectorXd z = line.row(index).head<2>();
 
-                yi(i) = r(i) - qi.col(i) * V.col(i);
-            }
 
-            //step 3
+				//r
 
-            Eigen::Matrix<float, 2, 2> M;
-            M << 0, -1,
-                 1, 0;
+					r(k) = abs(qi.col(k).dot(z));
 
-            Eigen::Matrix<double, 2, 1> vm(prevX, prevY);
 
-            Eigen::VectorXf xi3(400);
-            for (int g = 0; g < 400; g++) {
-                xi3(g) = qi.col(g).transpose() * M * (V.col(g) - vm);
-            }
+				//yi
+					yi(k) = r(k) - abs(qi.col(k).dot(V.col(k)));
 
-            Eigen::VectorXf tempx = dist_All.row(0);
+			}
 
-            // Calculate the median using std::nth_element
-            auto middle = tempx.begin() + tempx.size() / 2;
-            std::nth_element(tempx.begin(), middle, tempx.end());
-            float threshold = *middle;
+			//step 3
 
-            std::vector<int> outliers;
-            for (int i = 0; i < tempx.size(); ++i) {
-                if (tempx(i) > threshold) {
-                    outliers.push_back(i);
-                }
-            }
+			Matrix2d M;
+			M << 0, -1, 1, 0;
 
-            // Remove outliers from the matrices
-            for (int i = outliers.size() - 1; i >= 0; --i) {
-                int index = outliers[i];
 
-                qi.col(index).swap(qi.col(qi.cols() - 1));
-                qi.conservativeResize(qi.rows(), qi.cols() - 1);
 
-                yi.col(index).swap(yi.col(yi.cols() - 1));
-                yi.conservativeResize(yi.rows(), yi.cols() - 1);
+			MatrixXd vm{
+					{prevX+ddx},
+					{prevY+ddy},
+			};
 
-                r.col(index).swap(r.col(r.cols() - 1));
-                r.conservativeResize(r.rows(), r.cols() - 1);
 
-                xi3.col(index).swap(xi3.col(xi3.cols() - 1));
-                xi3.conservativeResize(xi3.rows(), xi3.cols() - 1);
+			MatrixXd xi3(1,x.cols());
+			xi3.row(0).setZero();
 
-                Xw.col(index).swap(Xw.col(Xw.cols() - 1));
-                Xw.conservativeResize(Xw.rows(), Xw.cols() - 1);
-            }
+			for (int g = 0; g < 400; g++) {
+				xi3(g) = qi.col(g).transpose() * M * (V.col(g) - vm);
+			}
 
-            Eigen::VectorXf xi1 = qi.row(0);
-            Eigen::VectorXf xi2 = qi.row(1);
+			VectorXd tempx = dist_All.row(0).transpose();
 
-            Eigen::MatrixXf A(tempx.size(), 3);
-            A.col(0) = xi1;
-            A.col(1) = xi2;
-            A.col(2) = xi3;
-            A.transpose();
+			// Calculate the median using std::nth_element
+			auto middle = tempx.begin() + tempx.size() / 2;
+			std::cout << tempx.transpose() << endl;
+			nth_element(tempx.begin(), middle, tempx.end());
+			std::cout << tempx.transpose() << endl;
+			double threshold = *middle;
+			std::cout << threshold << endl;
+			//double threshold = 18.7341;
 
-            Eigen::MatrixXf AtA = A.transpose() * A;
-            Eigen::MatrixXf AtYi = A.transpose() * yi.transpose();
-            Eigen::MatrixXf b = AtA.inverse() * AtYi;
+			vector<long long> outliers;
+			for (int j = 0; j < tempx.rows(); j++) {
+				if (tempx(j) > threshold) { //changed to contain inliers
+					outliers.push_back(j);
+				}}
+			for(int f=0; f<=outliers.size(); f++){
+				std::cout << outliers[f] << endl;
+			}
 
-            Eigen::VectorXf residual = yi - A * b;
 
-            float S2 = (residual.transpose() * residual) / (n - 4);
+			// Remove outliers from the matrices
+			for (int j = outliers.size()-1; j >= 0; j--) {
+				long long outlierIndex = outliers[j];
+				qi.col(outlierIndex).swap(qi.col(qi.cols()-1)); // Swap outlier column with last column
+				qi.conservativeResize(qi.rows(), qi.cols()-1); // Remove last column
+				yi.col(outlierIndex).swap(yi.col(yi.cols()-1));
+				yi.conservativeResize(yi.rows(), yi.cols()-1);
+				xi3.col(outlierIndex).swap(xi3.col(xi3.cols()-1));
+				xi3.conservativeResize(xi3.rows(), xi3.cols()-1);
+				Xw.col(outlierIndex).swap(Xw.col(Xw.cols()-1));
+				Xw.conservativeResize(Xw.rows(), Xw.cols()-1);
+			}
+			std::cout << yi << endl;
 
-            Eigen::MatrixXf C = S2 * (A.transpose() * A).inverse();
 
-            // Step 4
-            ddx += b(0);
-            ddy += b(1);
-            dda += b(2);
+			MatrixXd xi1 (1,qi.cols());
+			xi1 << qi.row(0);
+			MatrixXd xi2 (1,qi.cols());
+			xi2 << qi.row(1);
 
-            prevX = prevX + b(1);
-            prevY = prevY + b(2);
-            prevA = prevA + b(3);
 
-            // Step 5
-            if (sqrt(pow(b(0), 2) + pow(b(1), 2)) < 5 && abs(b(2)) < 0.1 * M_PI / 180) {
-                break;
-            }
+			MatrixXd A(3, qi.cols());
+			A.row(0) = xi1.row(0);
+			A.row(1) = xi2.row(0);
+			A.row(2) = xi3.row(0);
+			A.transposeInPlace();
+
+
+			MatrixXd AtA = A.transpose() * A;
+			MatrixXd AtYi = A.transpose() * yi.transpose();
+			MatrixXd b = AtA.inverse() * AtYi;
+
+			MatrixXd S2 = ((yi.transpose() - A * b).transpose() * (yi.transpose() - A * b)) / (400 - 4);
+			double s2 = S2(0,0);
+
+			MatrixXd C (3,3);
+			C << s2 * (A.transpose() * A).inverse();
+			//std::cout << " C\n";
+			//std::cout << C << std::endl;
+
+			// Step 4
+			ddx += b(0);
+			ddy += b(1);
+			dda += b(2);
+
+			std::cout << "b" << endl;
+			std::cout << b << endl;
+			std::cout << "X Y A" << endl;
+			std::cout << ddx << ' ' << ddy << ' ' << dda*180/M_PI << endl;
+			// Step 5
+			if (sqrt(pow(b(0), 2) + pow(b(1), 2)) < 5 && abs(b(2)) < 0.1 * M_PI / 180) {
+				std::cout << "break" << endl;
+				break;
+          /**/  }
 
         }
-    }
+    };
 
-//};
 
 double pointLineDistance(double x0, double y0, double x1, double y1, double x2, double y2) {
     double dist = abs((y2-y1)*x0 - (x2-x1)*y0 + x2*y1 - y2*x1) / sqrt(pow(y2-y1, 2) + pow(x2-x1, 2));
@@ -264,14 +295,9 @@ double pointLineDistance(double x0, double y0, double x1, double y1, double x2, 
 
 int main(void){
 	coxMap map;
-	//map.setup();
-	//std::cout << "Here are the vectors u1:\n" << map.ri[0]  << std::endl;
+	map.setup();
+	std::cout << "Here are the vectors u1:\n" << map.ri[0]  << std::endl;
 	map.gather_data();
-	map.loop(20);
-
-
-
-
-
-
+	map.loop(10);
+	cout << "Finished" << endl;
 }
